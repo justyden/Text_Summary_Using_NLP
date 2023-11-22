b33a8bb820780ef22b2a25e5437f94bf1b0dea62
afeins19 <afeinberg35@gmail.com>
2023-11-22 18:18:10 -0500

    models have docstrings

--- src/TextSummaryUsingNlp/TextAnalyzer/models.py
+++ src/TextSummaryUsingNlp/TextAnalyzer/models.py
@@ -6,6 +6,7 @@ MAX_TWEET_LENGTH = 280
 # Create your models here
 
 class RawTextDataModel(models.Model):
+    """Holds the raw text that the user would like to analyze"""
     raw_text = models.CharField(max_length = MAX_TWEET_LENGTH)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -13,6 +14,7 @@ class RawTextDataModel(models.Model):
         return str(self.raw_text[:50])
 
 class EmotionalAnalysisModel(models.Model):
+    """Holds Raw Text that this function was invoked with and emotional classification details"""
     raw_text = models.OneToOneField(RawTextDataModel, on_delete=models.CASCADE)
     emotion_category = models.CharField(max_length=50)
     analyzed_at = models.DateTimeField(auto_now_add=True)
@@ -21,6 +23,7 @@ class EmotionalAnalysisModel(models.Model):
         return str(self.emotion_category)
 
 class SentimentAnalysisModel(models.Model):
+    """Holds Raw text that this function was invoked for and sentiment analysis data (score)"""
     raw_text = models.OneToOneField(RawTextDataModel, on_delete=models.CASCADE)
     sentiment_score = models.FloatField()
     analyzed_at = models.DateTimeField(auto_now_add=True)
