c7c73453e92e039d8ee7cc379adb45b772e61669
Joseph Porrino <joeyporrino1998@gmail.com>
2023-12-09 22:28:27 -0500

    Added upload feature for PDF files

--- src/TextSummaryUsingNlp/TextAnalyzer/forms.py
+++ src/TextSummaryUsingNlp/TextAnalyzer/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from django.core.validators import FileExtensionValidator
 
 class TextInputForm(forms.Form):
     form_name = "Insert the text here"
-    text = forms.CharField(widget=forms.Textarea(attrs={'rows': 4, 'cols': 50}))
+    text = forms.CharField(widget=forms.Textarea(attrs={'rows': 4, 'cols': 50}), required=False)
+    upload_file = forms.FileField(required=False, validators=[FileExtensionValidator(['pdf'])])
 
--- src/TextSummaryUsingNlp/TextAnalyzer/views.py
+++ src/TextSummaryUsingNlp/TextAnalyzer/views.py
@@ -3,19 +3,26 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import TextInputForm
 from Helper import Helper
-
+import PyPDF2
 analysis = Helper()
+
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World!")
 
+
 def home(request):
     if request.method =='POST':
-        form = TextInputForm(request.POST)
+        form = TextInputForm(request.POST, request.FILES)
 
         if form.is_valid():
-            raw_text = form.cleaned_data['text']
+            if 'text' in request.POST and request.POST['text']:
+                raw_text = form.cleaned_data['text']
+            else:
+                upload_file = request.FILES.get('upload_file')
+                if upload_file:
+                    raw_text = extract_text_from_pdf(upload_file)
 
             # Perform text analysis using your Helper class
             summary = analysis.summarize_texts([raw_text])  # Pass a list containing the raw_text
@@ -32,10 +39,31 @@ def home(request):
             return render(request, 'TextAnalyzer/result.html', analyzed_data)
 
     else:
-        initial_text_value = ""  # Set your desired initial value
+        initial_text_value = ("Enter text here or upload a pdf file. Must be 512 characters or less. "
+                              "Delete this text before uploading a file.")  # Set your desired initial value
         form = TextInputForm(initial={'text': initial_text_value})
 
     return render(request, 'TextAnalyzer/home.html', {'form': form})
 
+
 def about(request):
     return render(request, 'TextAnalyzer/about.html')
+
+
+def extract_text_from_pdf(pdf_file):
+    with pdf_file.open('rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        num_pages = len(reader.pages)
+
+        text = ''
+        for page_num in range(num_pages):
+            page = reader.pages[page_num]
+            text += page.extract_text()
+
+    if len(text) > 512:
+        shortened_text = ""
+        for num in range(512):
+            shortened_text += text[num]
+        return shortened_text
+
+    return text
